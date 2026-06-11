0063f478e579878fe8df69c0539fc0c3f4d61db2
Arturo <parra.rivas.arturogabriel@gmail.com>
2026-06-10 20:05:40 -0600

    feat: correción de errores en la barra de búsqueda y botones de filtros y visualización de artícuos individuales

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,5 @@
-export type NewsCategory = 'all' | 'real'  | 'fake' /*| 'satire' */ | 'misleading' | 'none';
+export type NewsCategory = 'all' | 'real'  | 'fake' | 'satire'  | 'misleading' | 'none';
 
 export interface SearchFilters {
   text: string;
-  language: string;
-  country: string;
-  category: string;
-  dateFrom: string;
-  dateTo: string;
-  sourceDomain: string;
-  credibilityMin: number;
-  credibilityMax: number;
 }
